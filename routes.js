5d824e03e784eae552c1fc26de496b48fc67e4bf
Rina <Rina@WINDOWS-O4DFLS1>
2020-03-24 18:30:22 +0200

    some changes

--- routes.js
+++ routes.js
@@ -1,6 +1,6 @@
 const routes =
     {
-    '/Rina-Zabag-19-03-2020/': `<div class="home-page-container">
+    '/': `<div class="home-page-container">
     <div class="search-container">
         <i class="fas fa-search"></i>
         <input class="search-box" placeholder="Search For Country...">
@@ -63,7 +63,7 @@ const routes =
         </div>
         </div>
     </div>`,
-     '/Rina-Zabag-19-03-2020/favorites': `<div class="favorites">
+     '/favorites': `<div class="favorites">
     </div>`
     };
 
